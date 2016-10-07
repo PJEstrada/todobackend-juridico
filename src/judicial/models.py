9463d4e95fc74fc7463bcd379d27364b3162d324
Palo <pjestrada006@hotmail.com>
2016-10-07 13:28:07 -0600

    Agreagados OpinionJuridica y Dictamen como Entidades del Contexto Juridico

--- src/judicial/models.py
+++ src/judicial/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class ExpedienteJuridico(models.Model):
-
+	# Alguna otra informacion relevante al contexto juridico ?
 	documentos = models.ManyToManyField(Documento)
 	estado = models.ForeignKey(EstadoExpediente)
 	numero = models.IntegerField()
@@ -20,6 +20,7 @@ class Documento(models.Model):
 	ultima_modificacion = models.DateField()
 
 class Providencia(models.Model):
+	# La providencia contiene alguna otra informacion?
 	gerencia_destino = models.ForeignKey(Gerencia)
 	expediente = models.ForeignKey(ExpedienteJuridico)
 	asunto = models.CharField()
@@ -34,6 +35,15 @@ class Gerencia(models.Model):
 	nombre = models.CharField()
 	descripcion = models.CharField()
 
-class AnalistaJuridico(AbstractUser):
+class AsesorJuridico(AbstractUser):
 	nombre = models.CharField()
 	key = models.CharField()
+
+
+class Dictamen(models.Model):
+	# PENDIENTE: ....
+	...
+
+class OpinionJuridica(models.Model):
+	# PENDIENTE: ....
+	...
