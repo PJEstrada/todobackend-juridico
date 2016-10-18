1d515167f97b28bbdfc6b7e0cb8bedd38aa09738
Palo <pjestrada006@hotmail.com>
2016-10-17 20:46:21 -0600

    test

--- src/judicial/views.py
+++ src/judicial/views.py
@@ -10,7 +10,7 @@ from rest_framework.parsers import JSONParser
 from judicial.models import AsesorJuridico
 from judicial.serializer import AsesorSerializer
 
-# 6645455552aaa  cambio 589
+# 8886645455552aaa  cambio 589
 class JSONResponse(HttpResponse):
     """
     """
