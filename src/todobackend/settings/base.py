9daea3b622a6261c7e1159a514aee9903e1192c6
Palo <pjestrada006@hotmail.com>
2016-11-02 18:12:10 -0600

    Removing rest permissions

--- src/todobackend/settings/base.py
+++ src/todobackend/settings/base.py
@@ -74,13 +74,13 @@ TEMPLATES = [
     },
 ]
 
-REST_FRAMEWORK = {
+#REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+#    'DEFAULT_PERMISSION_CLASSES': [
+#        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#    ]
+#}
 
 WSGI_APPLICATION = 'todobackend.wsgi.application'
 
