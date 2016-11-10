8b28183de3bcb4cb8b715d87a5eefbbefc25f5bd
Palo <pjestrada006@hotmail.com>
2016-11-09 19:25:44 -0600

    cors

--- src/todobackend/settings/base.py
+++ src/todobackend/settings/base.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
