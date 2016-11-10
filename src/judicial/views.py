30a62b78021e82207463d61fe455c2d85b0bcad0
Palo <pjestrada006@hotmail.com>
2016-11-10 16:59:50 -0600

    Added tests

--- src/judicial/views.py
+++ src/judicial/views.py
@@ -173,7 +173,6 @@ def emitir_providencia(request):
             instance.save()
             #TODO cambiar el estado al expediente
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        print serializer.errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
